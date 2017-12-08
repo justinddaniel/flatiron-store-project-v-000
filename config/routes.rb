9e557ec2e73c03c4d4cebf6f00f1ed0ca07e6c43
Justin Daniel <justinddaniel@gmail.com>
2017-12-08 23:55:52 +0000

    made sessions controller and sessions routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'store#index', as: 'store'
 
+  #sessions -newly created
+  get '/signin', as: 'sessions#new'
+  post "/sessions/create", as: "sessions#create"
+  delete "/signout", as: "sessions#destroy"
+
+  #pre-made resources
   resources :items, only: [:show, :index]
   resources :categories, only: [:show, :index]
   resources :users, only: [:show]
