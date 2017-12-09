3b29b48b28b20741cff0d38915483e897784efe7
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-12-08 22:11:26 -0500

    Reformatted index and category show page to use partial with a collection Completed Category and Store_Home specs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,8 @@
 class CategoriesController < ApplicationController
+  def show
+    @user = current_user
+    @category = Category.find_by(id: params[:id])
+    @items = @category.items
+  end
 
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,7 @@ class StoreController < ApplicationController
   def index
     @user = current_user
     @categories = Category.all
-    @items = Item.all
+    @available_items = Item.available_items
   end
 
 end
