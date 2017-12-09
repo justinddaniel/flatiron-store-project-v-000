87d56ba6c126d15d7a95b794416db6f5b40479a1
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-12-08 20:43:50 -0500

    Added attr accessor for current_cart to pass test and give functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :current_cart
+  
   has_many :carts
   has_many :orders
-  belongs_to :current_cart, :class_name => "Cart"
 
-  def create_current_cart
-    new_cart = carts.create
-    self.current_cart_id = new_cart.id
-    save
-  end
+  # def create_current_cart
+  #   new_cart = carts.create
+  #   self.current_cart_id = new_cart.id
+  #   save
+  # end
 
-  def remove_cart
-    self.current_cart_id = nil
-    save
-  end
+  # def remove_cart
+  #   self.current_cart_id = nil
+  #   save
+  # end
+
+
 
 end
