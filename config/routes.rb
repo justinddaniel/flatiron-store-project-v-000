5ec7ce96abdc360f809f5a68d9ec635d4cc43ec9
Justin Daniel <justinddaniel@gmail.com>
2017-12-09 01:12:21 +0000

    edited header. Working on store_home specs. Routes need editing.

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store'
 
   #sessions -newly created
-  get '/signin', as: 'sessions#new'
-  post "/sessions/create", as: "sessions#create"
-  delete "/signout", as: "sessions#destroy"
+  get '/signin', to: 'sessions#new'
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+
+  #signup pathway
+  get '/signup', to: 'users#new'
 
   #pre-made resources
   resources :items, only: [:show, :index]
   resources :categories, only: [:show, :index]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :show]
   resources :carts
   resources :line_items, only: [:create]
   resources :orders, only: [:show]
